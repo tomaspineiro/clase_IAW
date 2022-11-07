3b788ad5a6836bb56273063a18c58ca12f3e7825
tomaspineiro <tomaspial95@gmail.com>
2022-11-07 13:27:50 +0100

    sddf

--- PHP/Formularios/procesa1.php
+++ PHP/Formularios/procesa1.php
@@ -6,11 +6,30 @@
 	<body>
 		<h1>Boletin  de PHP</h1>
 		<h5>ejercio  </h5>
-		<?php
-			
-			print_r($_GET);
-			
-		?>		
+		<!--
+			<?php			
+				print_r($_GET);
+			?>	
+		-->
+		<!--
+			<?php			
+				print_r($_REQUEST);
+			?>	
+		-->
+		
+		<p>Nombre: <?php echo $_REQUEST['nombre']; ?> </p>
+		<p>Password: <?php echo $_REQUEST['password']; ?></p>
+		<p>Edad: <?php echo $_REQUEST['edad']; ?></p>
+		<p>Aficiones:
+			<?php 
+				foreach ($_REQUEST['aficiones'] as $i ){
+					echo $i . ' ';
+				}
+			?>
+		</p>
+		<p></p>
+		<p></p>
+		<p></p>
 			
 	</body>
 </html>
