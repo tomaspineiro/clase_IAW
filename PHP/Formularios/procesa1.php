6d8aa96abc62deb81389ab16eb341e521aedaf98
tomaspineiro <tomaspial95@gmail.com>
2022-11-07 13:05:00 +0100

    formulario

---
+++ PHP/Formularios/procesa1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Procesado 1</title>
+	</head>
+	<body>
+		<h1>Boletin  de PHP</h1>
+		<h5>ejercio  </h5>
+		<?php
+			
+			print_r($_GET);
+			
+		?>		
+			
+	</body>
+</html>
