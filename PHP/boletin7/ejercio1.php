b0b2ba2c5b51e315320432d836cc7ebbec6dfc60
tomaspineiro <tomaspial95@gmail.com>
2022-11-14 13:26:37 +0100

    b7 eje1
    plantila

---
+++ PHP/boletin7/ejercio1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Boletin 7 de PHP</title>
+	</head>
+	<body>
+		<h1>Boletin 7 de PHP</h1>
+		<h5>ejercio 1 </h5>
+		<?php
+			//
+			
+		?>		
+			
+	</body>
+</html>
