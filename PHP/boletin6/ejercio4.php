3ed520046066f5abdfab12e617223505d8dec189
tomaspineiro <tomaspial95@gmail.com>
2022-11-28 13:38:45 +0100

    Correcion de los ejercios

--- PHP/boletin6/ejercio4.php
+++ PHP/boletin6/ejercio4.php
@@ -1,17 +1,17 @@
 <?php include('funciones.php'); ?>
-<?php function formulario() { ?>
+<?php function formulario($nota1 = 0, $nota2 = 0 , $nota3 = 0) { ?>
 	<form action="" method="GET">
 		<p>
 			<label for="num1">Numero 1:</label>
-			<input type="number" name="num1" id="num1">
+			<input type="number" name="num1" id="num1" value="<?= $nota1?>">
 		</p>
 		<p>
 			<label for="num2">Numero 2:</label>
-			<input type="number" name="num2" id="num2">
+			<input type="number" name="num2" id="num2" value="<?= $nota2?>">
 		</p>
 		<p>
 			<label for="num3">Numero 3:</label>
-			<input type="number" name="num3" id="num3">
+			<input type="number" name="num3" id="num3" value="<?= $nota3?>">
 		</p>
 		<p>
 			<button type="submit" name="enviar" id='enviar'>Enviar</button>
@@ -40,6 +40,35 @@
 				$nota1 = recoge('num1');
 				$nota2 = recoge('num2');
 				$nota3 = recoge('num3');
+				
+				$errores = "";
+
+				if ($nota1 != "") {
+
+					$errores .= "<li> no puede quedar en blanco la nota1 "; 
+				}
+
+				if ($nota2 != "") {
+
+					$errores .= "<li> no puede quedar en blanco la nota2 "; 
+				}
+
+		
+				if ($nota3 != "") {
+
+					$errores .= "<li> no puede quedar en blanco la nota3 "; 
+				}
+
+				$nota1 = (int)$nota1;
+				$nota2 = (int)$nota2;
+				$nota3 = (int)$nota3;
+				
+				if ($errores != ""){
+					
+					
+					formulario($nota1, $nota2, $nota3);
+				}
+
 				$media = ($nota1 + $nota2 + $nota3) / 3;	
 			?>
 				<p>
