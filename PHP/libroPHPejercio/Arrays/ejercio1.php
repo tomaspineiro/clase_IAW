a78e331af36b5f93aacc7a0b53d4269d8892aa07
tomaspineiro <tomaspial95@gmail.com>
2022-12-02 13:32:06 +0100

    re

--- PHP/libroPHPejercio/Arrays/ejercio1.php
+++ PHP/libroPHPejercio/Arrays/ejercio1.php
@@ -9,7 +9,8 @@
 <body>
     <section id="Enuciado">
         <article>
-            <h1>Ejercio  del Tema 5</h1>
+            <h1>Ejercios  del Tema 5</h1> 
+            <h3>Ejercio 1</h3>
             <p>
                 Define tres arrays de 20 números enteros cada una, con nombres “numero”, “cuadrado” y “cubo”. Carga el array “numero” con valores aleatorios entre 0 y 100. En el array “cuadrado” se deben almacenar los cuadrados de los valores que hay en el array “numero”. En el array “cubo” se deben almacenar los cubos de los valores que hay en “numero”. A continuación, muestra el contenido de los tres arrays dispuesto en tres columnas.  
             </p>
