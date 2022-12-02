397262e4af4f8953df8e5a59c6503ee9968f1637
tomaspineiro <tomaspial95@gmail.com>
2022-12-02 13:31:16 +0100

    estructura de arrays

---
+++ PHP/libroPHPejercio/Arrays/ejercio1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercio del Libro: Tema 5</title>
+</head>
+<body>
+    <section id="Enuciado">
+        <article>
+            <h1>Ejercio  del Tema 5</h1>
+            <p>
+                Define tres arrays de 20 números enteros cada una, con nombres “numero”, “cuadrado” y “cubo”. Carga el array “numero” con valores aleatorios entre 0 y 100. En el array “cuadrado” se deben almacenar los cuadrados de los valores que hay en el array “numero”. En el array “cubo” se deben almacenar los cubos de los valores que hay en “numero”. A continuación, muestra el contenido de los tres arrays dispuesto en tres columnas.  
+            </p>
+        </article>
+        <hr>
+    </section>
+
+</body>
+</html>
---
+++ PHP/libroPHPejercio/Arrays/ejercio4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercio del Libro: Tema 5</title>
+</head>
+<body>
+    <section>
+        <article>
+            <h1>Ejercio  del Tema 5</h1>
+            <p>
+                
+            </p>
+        </article>
+    </section>
+</body>
+</html>
