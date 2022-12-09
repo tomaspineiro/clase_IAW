95f8540ab3d0207325a0c60247bcdbe6e9e630cb
tomaspineiro <tomaspial95@gmail.com>
2022-12-09 14:47:20 +0100

    boletin 1

---
+++ cookiesSesiones/boletin1/if-else-1-3.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * if ... else ... (1) 3 - if-else-1-3.php
+ *
+ * @author Tomás Piñeiro Alonso
+ *
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Dos dados más altos. Juego.
+    if .. elseif ... else ... (1). Sin formularios.
+    Tomás Piñeiro Alonso
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Juego: Dos dados más altos</h1>
+
+  <p>Actualice la página para mostrar una nueva tirada.</p>
+
+  <table>
+    <tr>
+      <th>Jugador 1</th>
+      <th>Jugador 2</th>
+      <th>Resultado</th>
+    </tr>
+    <tr>
+<?php
+
+print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+
+?>
+    </tr>
+  </table>
+
+  <footer>
+    <p>Tomás Piñeiro Alonso</p>
+  </footer>
+</body>
+</html>
