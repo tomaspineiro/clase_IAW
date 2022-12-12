b760f55e3f73df8a7fa6cc4dc6b92e8e8849be7b
tomaspineiro <tomaspial95@gmail.com>
2022-12-12 14:41:21 +0100

    ACABADO

---
+++ cookiesSesiones/boletin1/borrarCookie.php
@@ -0,0 +1,8 @@
+<?php
+
+setcookie('jugador1', 0, time()-60*60*2);
+setcookie('jugador2', 0, time()-60*60*2);
+setcookie('empates', 0, time()-60*60*2);
+setcookie('tiradas', 0, time()-60*60*2);
+
+?>
