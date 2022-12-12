9f1429dd37f120c85203b0a0e8a9017437773c1b
tomaspineiro <tomaspial95@gmail.com>
2022-12-12 14:58:41 +0100

    inicio, sin crear sesiones y gcalculo de ganador

--- cookiesSesiones/boletin1/if-else-1-3.php
+++ cookiesSesiones/boletin1/if-else-1-3.php
@@ -1,3 +1,5 @@
+<?php include "funciones.php"; ?>
+<?php session_start(); ?>
 <?php
 /**
  * if ... else ... (1) 3 - if-else-1-3.php
@@ -27,23 +29,49 @@
     <p>Actualice la página para mostrar una nueva tirada.</p>
     <a href="https://www.mclibre.org/consultar/php/ejercicios/sin-formularios/if-else-1/index.html#ejercicio-2#ejercicio-3">Enunciado</a>
   </section>
-  <table>
-    <tr>
-      <th>Jugador 1</th>
-      <th>Jugador 2</th>
-      <th>Resultado</th>
-    </tr>
-    <tr>
-<?php
-
-print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
-
-?>
-    </tr>
-  </table>
-
-  <footer>
-    <p>Tomás Piñeiro Alonso</p>
-  </footer>
-</body>
-</html>
+  <section>
+    <?php 
+    $resultadoTirado = tiradaDados(4);
+    $jugador1 = $resultadoTirado[0] + $resultadoTirado[1];
+    $jugador2 = $resultadoTirado[2] + $resultadoTirado[3];
+    $empates = 0;
+    $tiradas = 0
+    ?>
+    <table>
+      <tr>
+        <th>Jugador 1</th>
+        <th>Jugador 2</th>
+        <th>Resultado</th>
+      </tr>
+      <tr>
+        <td bgcolor="blue">
+          <img src="./img/<?= $resultadoTirado[0]; ?>.svg" alt="">
+          <img src="./img/<?= $resultadoTirado[1]; ?>.svg" alt="">
+        </td>
+        <td bgcolor="red">
+          <img src="./img/<?= $resultadoTirado[2]; ?>.svg" alt="">
+          <img src="./img/<?= $resultadoTirado[3]; ?>.svg" alt="">
+        </td>
+        <td>
+          <ul>
+            <li>jugador1: <?= $jugador1; ?></li>
+            <li>jugador2: <?= $jugador2; ?></li>
+            <li>Empates: <?= $empates; ?></li>
+            <li>Tiradas Total: <?= $tiradas; ?></li>
+          </ul>  
+        </td>
+      </tr>
+    </table>
+    <form action="if-else-1-3.php" method="post">
+      <p>
+        <input type="submit" value="Tirar!" name="tirar" id="tirar"> 
+        <input type="submit" value="reiniciar" name="reiniciar" id="reiniciar">
+      </p>
+    </form>
+  </section>
+    <footer>
+      <p>Tomás Piñeiro Alonso</p>
+    </footer>
+  </body>
+  </html>
+  
